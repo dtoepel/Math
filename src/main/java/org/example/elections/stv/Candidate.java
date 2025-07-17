3e943439c3f7c2843db882156c3d2b3a5c167685
Daniel Töpel <toepel@gmx.it>
2025-07-17 18:09:13 +0200

    imported STV algorithm

---
+++ src/main/java/org/example/elections/stv/Candidate.java
@@ -0,0 +1,33 @@
+package org.example.elections.stv;
+
+import java.util.Comparator;
+
+public class Candidate {
+    public Candidate(String name) {
+        this.name = name;
+        this.random= Math.random();
+    }
+    public transient double weight = 1.0;
+    public transient CandidateStatus status = CandidateStatus.HOPEFUL;
+    public String name;
+    public double random;
+
+    public static class Sorter implements Comparator<Candidate> {
+        private Count count;
+
+        public Sorter(Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public int compare(Candidate c1, Candidate c2) {
+            if(count.voteCount.get(c1) > count.voteCount.get(c2)) return 1;
+            if(count.voteCount.get(c1) < count.voteCount.get(c2)) return -1;
+            if(c1.random > c2.random) return 1;
+            if(c1.random < c2.random) return -1;
+            return 0;
+        }
+
+    }
+
+}
